12e85b8a375d392cb4be274cefd9e12f234fce1e
Hillmii <hilmihidayatt64@gmail.com>
2023-11-15 21:36:42 +0700

    TailwindCss Semangka

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      ringWidth: {
+        '10': '15px',
+      }
+    },
+  },
+  plugins: [],
+}
+
+
